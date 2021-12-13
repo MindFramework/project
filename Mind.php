9b5d507f6078065bf20b1e387ce87a5cf1fedbec
Ali Yılmaz <aliyilmaz.work@gmail.com>
2021-12-13 10:03:33 +0300

    Mind was upgraded to version 4.8.1.

--- Mind.php
+++ Mind.php
@@ -2,7 +2,7 @@
 /**
  *
  * @package    Mind
- * @version    Release: 4.8.0 (For 4.7.9 and earlier: https://github.com/aliyilmaz/Mind)
+ * @version    Release: 4.8.1 (For 4.7.9 and earlier: https://github.com/aliyilmaz/Mind)
  * @license    GPL3
  * @author     Ali YILMAZ <aliyilmaz.work@gmail.com>
  * @category   Php Framework, Design pattern builder for PHP, New generation composer.
@@ -15,27 +15,39 @@ class Mind
 
     public static $conf;
     public static $path;
-    public static $developer;
-    public static $language;
     public static $class;
-    public static $version;
-    public static $params;
+    public static $classFile;
+    public static $developer;
 
     public static function __callStatic($method, $params) {
 
-        if(!isset($params[1])){ $params[1] = null; }
+        // Accessing another source code included in the package
+        if(strstr($params[0], ':')){ 
+            list($params[0], $callClass) = explode(':', $params[0]); }
 
+        // Developer name and Package name are converted to path
         self::$path = trim(implode('/', [$method, $params[0]]), '/');
 
-        list(self::$developer, self::$language, self::$class, self::$version) = explode('/', self::$path);
+        // Updating Developer name and Class name
+        list(self::$developer, self::$class) = explode('/', self::$path);
 
-        require_once('./developer/'.self::$path.'/src/'.self::$class.'.php');
-
-        self::$params = $params[1];
-
-        $class = new self::$class( self::$conf, $params[1] );
+        // If there is no special case, the existing package file is defined
+        if(empty($callClass)){ 
+            $callClass = self::$class; }
         
-        return $class;
+        // The file path of the request is created
+        self::$classFile = './developer/'.self::$path.'/src/'.$callClass.'.php';
+
+        // The package file is included
+        if(file_exists(self::$classFile)){
+           require_once(self::$classFile);
+        }
+
+        // If the class exists, it is defined.
+        if(class_exists($callClass)){
+            $class = new $callClass(self::$conf);
+            return $class;
+        }     
 
     }
     
--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
 
 require_once('Mind.php');
-
 $m = new Mind();
